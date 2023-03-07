6f7482a30439d834641ffa8069973693d9243f1c
rapan062 <rakshya.panta@javra.com>
2023-03-07 21:10:10 +0545

    add favicon and format output

--- myapp/views.py
+++ myapp/views.py
@@ -30,17 +30,18 @@ def result(request):
                 output_vanilla = model_vanilla(text).numpy()
                 predicted_vanilla = output_vanilla.decode('utf-8')
 
-                if (';' in predicted_vanilla):
-                    predicted_vanilla = predicted_vanilla.replace(';', ';\n')
-                elif (')' in predicted_vanilla):
-                    predicted_vanilla = predicted_vanilla.replace(')', ')\n')
-                elif ('{' in predicted_vanilla):
-                    predicted_vanilla = predicted_vanilla.replace('{', '{\n')
-                elif ('}' in predicted_vanilla):
-                    predicted_vanilla = predicted_vanilla.replace('}', '}\n')
-                else:
-                    predicted_vanilla = predicted_vanilla.replace('>', '>\n')
-                    print('Predicted Output: ', predicted_vanilla)
+                # if (';' in predicted_vanilla):
+                predicted_vanilla = predicted_vanilla.replace(';', ';\n')
+                # elif (')' in predicted_vanilla):
+                predicted_vanilla = predicted_vanilla.replace(')', ')\n')
+                # elif ('{' in predicted_vanilla):
+                predicted_vanilla = predicted_vanilla.replace('{', '{\n')
+                # elif ('}' in predicted_vanilla):
+                predicted_vanilla = predicted_vanilla.replace('}', '}\n')
+                # else:
+                predicted_vanilla = predicted_vanilla.replace('< i', '<i').replace('\r', '')
+                predicted_vanilla = predicted_vanilla.replace('m >', 'm>\n')
+                #     print('Predicted Output: ', predicted_vanilla)
                 b = time.perf_counter()
                 # print("Generated output: ", predicted )
 
@@ -59,7 +60,7 @@ def result(request):
                 model = CodeT5()
                 # print(tokenizer([text]))
                 torch.save(model.state_dict(
-                ), '/home/rakshya/Documents/MajorProjectFrontend/CodeT5Model/model_weights.pth')
+                ), 'CodeT5Model/model_weights.pth')
                 # input_tensor = torch.tensor(description)
                 # model.load_state_dict(torch.load('/home/rakshya/Documents/MajorProjectFrontend/CodeT5Model/model_weights.pth'))
                 # print(model.eval())
@@ -67,15 +68,18 @@ def result(request):
                 input_ids = tokenizer(text, return_tensors='pt').input_ids
                 attention_mask = tokenizer(text, return_tensors='pt').attention_mask
                 model_t5 = T5ForConditionalGeneration.from_pretrained(
-                    '/home/rakshya/Documents/MajorProjectFrontend/CodeT5Model/')
+                    'CodeT5Model')
                 # print(model_t5.eval())
                 output_t5 = model_t5.generate(input_ids, max_length=500)
                 predicted_t5 = tokenizer.decode(output_t5[0], skip_special_tokens=True)
+                predicted_t5 = predicted_t5.replace('\n', ' ').replace('\r', '')
+                predicted_t5 = predicted_t5.replace(' ;', ';').replace('< i', '<i').replace('m >', 'm> \n').replace('using','\n using')
+                predicted_t5 = predicted_t5.replace(';', ';\n').replace('{', '{\n')
+
 
                 b = time.perf_counter()
 
                 print('t5 - time to generate & display output:  ', round(b-a), " sec\n")
-
 
                 return render(request, 'result_t5.html', {'ques': text, 'result_t5': predicted_t5})
             except ValueError:
